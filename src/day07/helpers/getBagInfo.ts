5a550844485b8186385d66b7dabfd59d07ca40a7
unknown <norbitrial8@gmail.com>
2020-12-07 22:21:57 +0100

    Refactored getBagInfo

--- src/day07/helpers/getBagInfo.ts
+++ src/day07/helpers/getBagInfo.ts
@@ -1,19 +1,20 @@
+const BAG = ' bag'
+const SPACE = ' '
+
 type BagInfo = {
   amount: number
   color: string
 }
 
 const getBagInfo = (value: string): BagInfo => {
-  const [amount, ...rest] = value.split(' ')
-  let color = rest.join(' ')
-  const indexOfBag = color.indexOf(' bag')
-  color = color.substring(0, indexOfBag)
-  let parsedAmount = parseInt(amount)
-  parsedAmount = isNaN(parsedAmount) ? 0 : parsedAmount
+  const [amount, ...rest] = value.split(SPACE)
+  const color = rest.join(SPACE)
+  const indexOfBag = color.indexOf(BAG)
+  const parsedAmount = parseInt(amount)
 
   return {
-    amount: parsedAmount,
-    color,
+    amount: isNaN(parsedAmount) ? 0 : parsedAmount,
+    color: color.substring(0, indexOfBag),
   }
 }
 
