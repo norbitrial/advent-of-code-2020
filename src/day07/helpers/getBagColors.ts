b8da6c1d6d666b1223d3e7b17713230a99796d47
unknown <norbitrial8@gmail.com>
2020-12-07 20:33:37 +0100

    Removed not used files

--- src/day07/helpers/getBagColors.ts
+++
@@ -1,6 +0,0 @@
-import getBagColorsFromLine from './getBagColorsFromLine'
-
-const getBagColors = (lines: Array<string>): Array<string> =>
-  lines.flatMap((line) => getBagColorsFromLine(line))
-
-export default getBagColors
--- src/day07/helpers/getBagColorsFromLine.ts
+++
@@ -1,5 +0,0 @@
-const getBagColorsFromLine = (line: string): Array<string> => {
-  return []
-}
-
-export default getBagColorsFromLine
