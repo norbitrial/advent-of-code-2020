8ba1cbef7c9013678c578603edd9c6c92dcb3cbc
unknown <norbitrial8@gmail.com>
2020-12-08 22:44:55 +0100

    Solved part2 for day08

--- src/day08/helpers/isInfiniteLoop.ts
+++ src/day08/helpers/isInfiniteLoop.ts
@@ -1,35 +1,31 @@
 const SPACE = ' '
 
-const part01 = (instructionList: Array<string>): number => {
-  let accumulator = 0
+const isInfiniteLoop = (instructionList: Array<string>): boolean => {
   const alreadyTriedSteps = new Set()
   let index = 0
+  let stepsCount = 0
 
   while (true) {
     if (index >= instructionList.length) {
-      return accumulator
+      return false
     }
 
     const [action, value] = instructionList[index].split(SPACE)
 
     if (alreadyTriedSteps.has(index)) {
-      break
+      return stepsCount !== instructionList.length - 1
     }
 
     alreadyTriedSteps.add(index)
-
-    if (action === 'acc') {
-      accumulator = accumulator + parseInt(value)
-    }
 
     if (action === 'jmp') {
       index = index + parseInt(value)
     } else {
       index++
     }
-  }
 
-  return accumulator
+    stepsCount++
+  }
 }
 
-export default part01
+export default isInfiniteLoop
