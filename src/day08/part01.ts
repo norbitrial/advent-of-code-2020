7bf1719e78fe7e5b5e28c1f51084b9d7dd170350
unknown <norbitrial8@gmail.com>
2020-12-08 20:39:31 +0100

    Adding input, solution for part1 of day08

--- src/day08/part01.ts
+++ src/day08/part01.ts
@@ -1,3 +1,33 @@
-const part01 = (input: Array<string>): void => {}
+const SPACE = ' '
+
+const part01 = (instructionList: Array<string>): number => {
+  let accumulator = 0
+  const alreadyTriedSteps = new Set()
+  let index = 0
+
+  while (true) {
+    const [action, value] = instructionList[index].split(SPACE)
+
+    console.log({ action, value })
+
+    if (alreadyTriedSteps.has(index)) {
+      break
+    }
+
+    alreadyTriedSteps.add(index)
+
+    if (action === 'acc') {
+      accumulator = accumulator + parseInt(value)
+    }
+
+    if (action === 'jmp') {
+      index = index + parseInt(value)
+    } else {
+      index++
+    }
+  }
+
+  return accumulator
+}
 
 export default part01
